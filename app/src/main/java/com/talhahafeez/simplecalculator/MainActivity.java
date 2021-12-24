af05244673761a579773975a64117c57312f39bf
Shan Zafar <bitf18545@pucit.edu.pk>
2021-12-24 16:48:08 +0500

    A commit

--- app/src/main/java/com/talhahafeez/simplecalculator/MainActivity.java
+++ app/src/main/java/com/talhahafeez/simplecalculator/MainActivity.java
@@ -3,12 +3,51 @@ package com.talhahafeez.simplecalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        EditText num1, num2;
+        Button add, sub, mul, div;
+        TextView tv;
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+            num1 = findViewById(R.id.num1);
+            num2 = findViewById(R.id.num2);
+            add = findViewById(R.id.add);
+            sub = findViewById(R.id.sub);
+            mul = findViewById(R.id.mul);
+            div = findViewById(R.id.div);
+            tv = findViewById(R.id.textview1);
+            add.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                    int f, s, ans;
+                    f = Integer.parseInt(num1.getText().toString());
+                    s = Integer.parseInt(num2.getText().toString());
+                    ans = f + s;
+                    tv.setText("Answer = " +ans);
+
+                }
+            });
+            sub.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                    int f, s, ans;
+                    f = Integer.parseInt(num1.getText().toString());
+                    s = Integer.parseInt(num2.getText().toString());
+                    ans = f - s;
+                    tv.setText("Answer = " +ans);
+
+                }
+            });
     }
 }
